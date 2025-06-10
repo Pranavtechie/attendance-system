767167a7e438c44d1e3fbfcac61cafe4244678b7
Pranav Mandava <pranav.techiegeek@gmail.com>
2025-06-10 11:46:58 +0530

    flask test

---
+++ src/api/test.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+import requests as req
+import time
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return {"message": "Hello, World!"}
+
+
+@app.route("/test", methods=["GET"])
+def test():
+    r = req.get('https://api.github.com/events')
+    
+    return r.json()
+
+
+# new request to return the current timestamp in milliseconds
+@app.route("/timestamp", methods=["GET"])
+def timestamp():
+    return str(int(time.time() * 1000)) 
